ff34d719b7b60a386065915fd4959d7ca069682f
JGNIETOP <jgnietop_1@uqvirtual.edu.co>
2021-11-14 12:55:35 -0500

    Creación clase Safe_pet

---
+++ src/safe_pet/Safe_pet.java
@@ -0,0 +1,14 @@
+
+package safe_pet;
+
+
+public class Safe_pet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
